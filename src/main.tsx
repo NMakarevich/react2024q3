a6df63a1b2c9a01a8b62897759e52eb98637d7fc
NMakarevich <nikoladze529@mail.ru>
2024-07-09 23:10:37 +0300

    feat: start implementing routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { ErrorBoundary } from './components/error-boundary/error-boundary.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFound from './components/not-found/not-found.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <RouterProvider router={router} />
     </ErrorBoundary>
   </React.StrictMode>,
 );
