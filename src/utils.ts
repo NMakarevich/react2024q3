4d527a0f25a7bfb46f9c9b29461144191b633bb3
NMakarevich <nikoladze529@mail.ru>
2024-07-12 19:21:44 +0300

    refactor: change param type

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
-export function transformStars(stars: number): string {
-  return stars >= 1000 ? `${Math.round(stars / 1000)}k` : `${stars}`;
+export function transformStars(stars: number | undefined): string {
+  return stars
+    ? stars >= 1000
+      ? `${Math.round(stars / 1000)}k`
+      : `${stars}`
+    : '0';
 }
