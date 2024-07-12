3ac5602bc17a447a9797a30bad1c0a09ce2655a4
NMakarevich <nikoladze529@mail.ru>
2024-07-12 12:12:30 +0300

    refactor: update router

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { ErrorBoundary } from './components/error-boundary/error-boundary.tsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from 'react-router-dom';
 import NotFound from './components/not-found/not-found.tsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <Navigate to={'/search'} />,
+  },
+  {
+    path: '/search',
     element: <App />,
     errorElement: <NotFound />,
   },
