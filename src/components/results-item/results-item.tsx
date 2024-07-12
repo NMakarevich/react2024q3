059485f1f06ce0d38ef25e6a62d9a9b2007d0554
NMakarevich <nikoladze529@mail.ru>
2024-07-12 17:44:52 +0300

    refactor: update result item layout

--- src/components/results-item/results-item.tsx
+++ src/components/results-item/results-item.tsx
@@ -1,5 +1,7 @@
-import { Result } from '../../App.tsx';
+import { PageContext, Result } from '../../App.tsx';
 import './results-item.scss';
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
 
 interface Props {
   result: Result;
@@ -7,37 +9,40 @@ interface Props {
 
 export function ResultsItem(props: Props) {
   const { result } = props;
+  const navigate = useNavigate();
+  const pageContext = useContext(PageContext);
+
+  function navigateTo() {
+    navigate(
+      `/search/details?owner=${result.owner.login}&name=${result.name}&page=${pageContext}`,
+    );
+  }
 
   return (
     <>
-      <div className={'card'}>
-        <div className={'card-header'}>
-          <div className={'card-header_left'}>
-            <img
-              className={'card-logo'}
-              src={result.owner.avatar_url}
-              alt={result.owner.login}
-            />
-            <div className={'card-info'}>
-              <a className={'card-link link'} href={result.html_url}>
-                <h2 className={'card-title'}>{result.name}</h2>
+      <div className={'card'} onClick={navigateTo}>
+        <div className={'card-left'}>
+          <img
+            className={'card-logo'}
+            src={result.owner.avatar_url}
+            alt={result.owner.login}
+          />
+          <div className={'card-info'}>
+            <a className={'card-link link'} href={result.html_url}>
+              <h2 className={'card-title'}>{result.name}</h2>
+            </a>
+            <div className={'card-author'}>
+              <h3 className={'card-author_title'}>Author:</h3>
+              <a className={'card-author_link link'} href={result.owner.url}>
+                {result.owner.login}
               </a>
-              <div className={'card-author'}>
-                <h3 className={'card-author_title'}>Author:</h3>
-                <a className={'card-author_link link'} href={result.owner.url}>
-                  {result.owner.login}
-                </a>
-              </div>
             </div>
           </div>
-          <div className={'card-header_right'}>
-            <span className={'card-stars'}>
-              Stars: {transformStars(result.stargazers_count)}
-            </span>
-          </div>
         </div>
-        <div className={'card-body'}>
-          <p className={'card-description'}>{result.description}</p>
+        <div className={'card-right'}>
+          <span className={'card-stars'}>
+            Stars: {transformStars(result.stargazers_count)}
+          </span>
         </div>
       </div>
     </>
