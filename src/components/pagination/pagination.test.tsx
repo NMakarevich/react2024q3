d8035ce012f04bbc8b38c7c29b872fbda431b1cb
NMakarevich <nikoladze529@mail.ru>
2024-07-26 22:33:28 +0300

    fix: fix tests

--- src/components/pagination/pagination.test.tsx
+++ src/components/pagination/pagination.test.tsx
@@ -1,32 +1,51 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
+import { IPageContext, PageContext } from '../../App.tsx';
+import { setupServer } from 'msw/node';
+import { http, HttpResponse } from 'msw';
+import { response } from '../../mock/mock.ts';
 import Pagination from './pagination.tsx';
+
+const server = setupServer(
+  http.get('https://api.github.com/search/repositories', () => {
+    return HttpResponse.json(response);
+  }),
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+const props: IPageContext = {
+  page: 1,
+  setPage: vi.fn(),
+};
 
 describe('Pagination', () => {
   it('Click on next page changes page to next', () => {
-    const { pathname } = window.location;
-    const url = `${pathname}?page=1`;
-    window.history.pushState({}, '', url);
     render(
-      <BrowserRouter>
-        <Pagination totalCount={40} />
-      </BrowserRouter>,
+      <MemoryRouter initialEntries={['?page=1']}>
+        <PageContext.Provider value={props}>
+          <Pagination totalCount={40} />
+        </PageContext.Provider>
+      </MemoryRouter>,
     );
     const nextButton = screen.getByRole('button', { name: /Next/i });
     fireEvent.click(nextButton);
-    expect(window.location.search).toBe('?page=2');
+    const paginationText = nextButton.previousElementSibling;
+    expect(paginationText?.textContent).toBe('2 of 2');
   });
   it('Click on prev page changes page to prev', () => {
-    const { pathname } = window.location;
-    const url = `${pathname}?page=2`;
-    window.history.pushState({}, '', url);
     render(
-      <BrowserRouter>
-        <Pagination totalCount={60} />
-      </BrowserRouter>,
+      <MemoryRouter initialEntries={['?page=2']}>
+        <PageContext.Provider value={props}>
+          <Pagination totalCount={40} />
+        </PageContext.Provider>
+      </MemoryRouter>,
     );
-    const prevButton = screen.getByText('Prev page');
+    const prevButton = screen.getByRole('button', { name: /Prev/i });
     fireEvent.click(prevButton);
-    expect(window.location.search).toBe('?page=1');
+    const paginationText = prevButton.nextElementSibling;
+    expect(paginationText?.textContent).toBe('1 of 2');
   });
 });
