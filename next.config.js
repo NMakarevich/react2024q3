9ad54af4a682118b4b52714d1431ac1237d7899e
NMakarevich <nikoladze529@mail.ru>
2024-08-11 17:00:20 +0300

    fix: add redirect from / to /search

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: './dist',
+  async redirects() {
+    return [{ source: '/', destination: '/search', permanent: true }];
+  },
 };
 
 export default nextConfig;
