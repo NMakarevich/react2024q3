bffd2b6ec7ab5aaa051120e27510cd41378cdd4b
NMakarevich <nikoladze529@mail.ru>
2024-08-16 15:21:39 +0300

    feat: implement controlled form

---
+++ src/pages/controlled-page/controlled-page.tsx
@@ -0,0 +1,10 @@
+import ControlledForm from '../../components/controlled-form/controlled-form.tsx';
+
+export default function ControlledPage() {
+  return (
+    <>
+      <h2 className="page-title">Controlled form</h2>
+      <ControlledForm />
+    </>
+  );
+}
