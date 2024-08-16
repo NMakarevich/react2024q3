9f599267cbf0640b822b738be8f329c3ace4cec2
NMakarevich <nikoladze529@mail.ru>
2024-08-16 21:49:54 +0300

    refactor: delete page titles

--- src/pages/controlled-page/controlled-page.tsx
+++ src/pages/controlled-page/controlled-page.tsx
@@ -1,10 +1,5 @@
 import ControlledForm from '../../components/controlled-form/controlled-form.tsx';
 
 export default function ControlledPage() {
-  return (
-    <>
-      <h2 className="page-title">Controlled form</h2>
-      <ControlledForm />
-    </>
-  );
+  return <ControlledForm />;
 }
--- src/pages/uncontrolled-page/uncontrolled-page.tsx
+++ src/pages/uncontrolled-page/uncontrolled-page.tsx
@@ -1,10 +1,5 @@
 import UncontrolledForm from '../../components/uncontrolled-form/uncontrolled-form.tsx';
 
 export default function UncontrolledPage() {
-  return (
-    <>
-      <h2 className="page-title">Uncontrolled form</h2>
-      <UncontrolledForm />
-    </>
-  );
+  return <UncontrolledForm />;
 }
